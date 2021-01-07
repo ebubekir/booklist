1fcefa1c897a3e282ae14a57c5350f821c6be900
Ebubekir Karanfil <karanfilebubekir@gmail.com>
2021-01-07 19:59:15 +0300

    Get form values.

--- app.js
+++ app.js
@@ -34,12 +34,38 @@ class UI {
             <td>${book.isbn}</td>
             <td><a href="#" class="btn btn-danger btn-sm delete">X</a></td>`;
         list.appendChild(row);
-    } 
+    }
+    
+    static clearFields()
+    {
+        document.querySelector('#title').value = "";
+        document.querySelector('#author').value = "";
+        document.querySelector('#isbn').value = "";
+    }
 }
 //  Store Class: Handle Storage
 
 //  Event: Display Books
 document.addEventListener('DOMContentLoaded', UI.displayBooks);
 //  Event: Add a Book
+document.querySelector('#book-form')
+        .addEventListener('submit', (e) => {
+            // Prevent actual submit
+            e.preventDefault();
+            // Get form values
+            const title = document.querySelector('#title').value;
+            const author = document.querySelector('#author').value;
+            const isbn = document.querySelector('#isbn').value;
+            
+            // Instatiate Book
+            const book = new Book(title, author, isbn);
+            console.log(book);
 
+            // Add Book to UI
+            UI.addBookToList(book);
+
+            // Clear Fields
+            UI.clearFields();
+
+        });
 // Event: Remove a Book
